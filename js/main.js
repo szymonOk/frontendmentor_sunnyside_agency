5fe0663c4ed822908ff288f462f4251624629c19
Szymon Okulowski <szymon.okulowski@gmail.com>
2021-12-28 21:53:11 +0100

    finish nav

--- js/main.js
+++ js/main.js
@@ -1,3 +1,17 @@
+document.addEventListener('DOMContentLoaded', function () {
+	const navbar = document.querySelector('.nav')
+
+	function addNavShadow() {
+		if (window.scrollY >= 100) {
+			navbar.classList.add('nav-bg')
+		} else {
+			navbar.classList.remove('nav-bg')
+		}
+	}
+
+	window.addEventListener('scroll', addNavShadow)
+})
+
 const btn = document.querySelector('.nav__btn')
 const navList = document.querySelector('.nav__list-mobile')
 const navItem = document.querySelectorAll('.nav__list-mobile-item')
